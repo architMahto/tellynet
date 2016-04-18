019e585ce678b32f0572ee0ac2025e13910f60a9
architMahto <architmahto@cmail.carleton.ca>
2016-04-17 21:06:39 -0600

    Added angular app to app.js. Included Angular App in index.html.

---
+++ public/javascripts/app.js
@@ -0,0 +1,5 @@
+;(function() {
+  'use strict';
+
+  angular.module('tellyNetApp', ["ui.router", "ngSanitize", "slick", "com.2fdevs.videogular", "com.2fdevs.videogular.plugins.controls", "com.2fdevs.videogular.plugins.overlayplay", "com.2fdevs.videogular.plugins.poster", "com.2fdevs.videogular.plugins.buffering"])
+}())
