25528dde3d7783ba18497999c440b715d003cda5
architMahto <architmahto@cmail.carleton.ca>
2016-04-17 20:02:29 -0600

    Added code to server.js to run on port 3000.

---
+++ server.js
@@ -0,0 +1,22 @@
+var express    = require('express');
+var logger     = require('morgan');
+var bodyParser = require('body-parser');
+var cors       = require('cors');
+var mongoose   = require('mongoose');
+var port       = process.env.PORT || 3000;
+// var routes     = require('./routes/routes');
+
+// Create express app
+var app = express();
+
+/* middleware */
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(cors());
+// app.use('/', routes);
+app.use(express.static(__dirname + './public'));
+
+app.listen(port, function () {
+  console.log("Server running on port:", port);
+});
