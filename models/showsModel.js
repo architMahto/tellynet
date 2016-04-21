84c29a5c7a83a61daa7c1800aa7d906cd5635f4a
architMahto <architmahto@cmail.carleton.ca>
2016-04-20 21:55:51 -0600

    Added specialFeatures array to showsModel Schema.

--- models/showsModel.js
+++ models/showsModel.js
@@ -1,11 +1,12 @@
-var mongoose   = require('mongoose'),
-    Schema     = mongoose.Schema,
-    showSchema = new mongoose.Schema({
-      name     : {type: String, required: true},
-      details  : {synopsis: String},
-      seasons  : [],
-      poster   : String,
-      network  : String
+var mongoose          = require('mongoose'),
+    Schema            = mongoose.Schema,
+    showSchema        = new mongoose.Schema({
+      name            : {type: String, required: true},
+      details         : {synopsis: String},
+      seasons         : [],
+      specialFeatures : [],
+      poster          : String,
+      network         : String
     });
 
 module.exports = mongoose.model('Show', showSchema);
