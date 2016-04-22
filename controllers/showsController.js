f21d91023166e5688de68297c27b229d9d88b793
architMahto <architmahto@cmail.carleton.ca>
2016-04-21 22:34:26 -0600

    Added code to reference network by ObjectID.

--- controllers/showsController.js
+++ controllers/showsController.js
@@ -5,7 +5,7 @@ module.exports = {
     byNetwork: function (req, res) {
       var networkID = req.params.id;
 
-      Show.find({network: networkID}, function (err, shows) {
+      Show.find({network: ObjectId(networkID)}, function (err, shows) {
         if (err) console.log(err);
         res.json(shows);
       });
