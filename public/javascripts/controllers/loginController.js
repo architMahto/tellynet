d81957c7a93bf79af408dd1f93cc8b4122ebc589
architMahto <architmahto@cmail.carleton.ca>
2016-04-25 15:50:12 -0600

    Added code so that user will not be redirect to /browse on state change.

--- public/javascripts/controllers/loginController.js
+++ public/javascripts/controllers/loginController.js
@@ -12,21 +12,23 @@
     loginCtrl.newUserData = {};
 
     $rootScope.$on('$stateChangeSuccess', function () {
-      console.log("State Change");
 
       loginCtrl.loggedIn = Auth.isLoggedIn();
 
       Auth.getUser()
         .then(function (response) {
           loginCtrl.user = response.data;
-          console.log("api/me route", response);
-          $location.path('/browse');
         })
     });
 
+    Auth.getUser()
+      .then(function (response) {
+        loginCtrl.user = response.data;
+        $location.path('/browse');
+      })
+
     // function to allow user to join tellynet
     loginCtrl.signUp = function () {
-      console.log(loginCtrl.newUserData);
       usersFactory.create(loginCtrl.newUserData)
         .then(function(response) {
           console.log("Response from server: ", response);
@@ -35,13 +37,10 @@
 
     // function to handle login
     loginCtrl.doLogin = function () {
-      console.log("Do Login");
-      console.log(loginCtrl.loginData);
 
       Auth.login(loginCtrl.loginData.email, loginCtrl.loginData.password)
         .then(function (response) {
           AuthToken.setToken(response.data.token);
-          console.log("Response from server", response);
           if (response.data.success) {$location.path('/browse');}
           else {loginCtrl.error = response.data.message;}
         })
--- public/javascripts/factories/usersFactory.js
+++ public/javascripts/factories/usersFactory.js
@@ -12,31 +12,26 @@
 
     // get all users
     userData.all = function () {
-      console.log("Getting all users");
       return $http.get(usersUrl)
     }
 
     // get single user
     userData.single = function (id) {
-      console.log("Getting single user data:", id);
       return $http.get(usersUrl + id);
     }
 
     // create a user
     userData.create = function (user) {
-      console.log("Creating user", user);
       return $http.post(usersUrl, user);
     }
 
     // update a user
     userData.update = function (id, userInfo) {
-      console.log("Updating user info for user: ", id);
       return $http.put(usersUrl + id, userInfo);
     }
 
     // deleting a user
     userData.delete = function (id) {
-      console.log("Deleting user: ", id);
       return $http.delete(usersUrl + id);
     }
 
